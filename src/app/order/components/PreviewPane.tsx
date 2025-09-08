d6b806f63278fe543ffb4c0eac61ddaf6cdd251c
naveen <notnaveen2@gmail.com>
2025-09-08 16:21:46 +0530

    updated height issue in mobile screen for preview pane

--- src/app/order/components/PreviewPane.tsx
+++ src/app/order/components/PreviewPane.tsx
@@ -22,7 +22,7 @@ export default function PreviewPane({
             className="text-white bg-black/70 h-screen w-screen fixed top-0 left-0 z-10 flex justify-center items-center flex-col gap-y-5"
           >
             <div
-              className="w-[275px] h-[825px] sm:w-[302.2px] sm:h-[906.6px] bg-black overflow-hidden pt-4 px-2"
+              className="w-[233.33px] h-[700px] sm:w-[302.2px] sm:h-[906.6px] bg-black overflow-visible my-2 pt-4 px-2"
               onClick={(e) => e.stopPropagation()}
             >
               {photos.length > 0 && (
@@ -34,7 +34,7 @@ export default function PreviewPane({
                         key={index}
                         src={url}
                         alt={`Photo ${index + 1}`}
-                        className="object-cover opacity-100 w-[252.31px] h-[159.61px] sm:w-[277.27px] sm:h-[175.39px]"
+                        className="object-cover opacity-100 w-[214.09px] h-[135.42px] sm:w-[277.27px] sm:h-[175.39px]"
                         onLoad={(e) =>
                           URL.revokeObjectURL(
                             (e.target as HTMLImageElement).src
@@ -50,7 +50,7 @@ export default function PreviewPane({
                 alt="clickd Logo"
                 height={125}
                 width={125}
-                className="cursor-pointer mb-7 sm:mb-10 mx-auto"
+                className="cursor-pointer mb-4 sm:mb-10 mx-auto"
               />
               <Image
                 src="/strip_prevew_footer.png"
@@ -73,10 +73,10 @@ export default function PreviewPane({
           </div>
         ) : selectedLayout.photos === 3 ? (
           <div
-            className="text-white bg-black/70 h-screen w-screen fixed top-0 left-0 z-10 flex justify-center items-center flex-col gap-y-5"
+            className="text-white bg-black/70 h-screen w-screen fixed top-0 left-0 z-10 flex justify-center items-center flex-col gap-y-5 "
             onClick={() => setViewPreviewPane(false)}
           >
-            <div className="w-[275.48px] h-[825px] sm:w-[302.73px] sm:h-[906.6px] bg-black overflow-hidden pt-4 px-2">
+            <div className="w-[233.74px] h-[700px] sm:w-[302.73px] sm:h-[906.6px] bg-black  overflow-visible my-2  pt-4 px-2">
               {photos.length > 0 && (
                 <div
                   className="mb-10 overflow-hidden flex items-center flex-col gap-y-2"
@@ -89,7 +89,7 @@ export default function PreviewPane({
                         key={index}
                         src={url}
                         alt={`Photo ${index + 1}`}
-                        className="object-cover opacity-100 w-[252.22px] h-[215.15px] sm:w-[277.17px] sm:h-[236.43px]"
+                        className="object-cover opacity-100 w-[214.01px] h-[182.56px] sm:w-[277.17px] sm:h-[236.43px]"
                         onLoad={(e) =>
                           URL.revokeObjectURL(
                             (e.target as HTMLImageElement).src
@@ -105,7 +105,7 @@ export default function PreviewPane({
                 alt="clickd Logo"
                 height={125}
                 width={125}
-                className="cursor-pointer mb-7 sm:mb-10 mx-auto"
+                className="cursor-pointer mb-5 sm:mb-10 mx-auto"
               />
               <Image
                 src="/strip_prevew_footer.png"
